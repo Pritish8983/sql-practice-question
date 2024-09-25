8b59ed58536947b9da6e08dd91ede3ee5759157d
unknown <pritishpatra567@gmail.com>
2024-09-25 12:46:22 +0530

    tenth commit

---
+++ Q.no10.sql
@@ -0,0 +1,4 @@
+--alter table Employee_details Rename to Employee_database
+
+--Alter table Employee_database Rename column Firstname to name;
+Alter table Employee_database Rename column lastname to surname;
