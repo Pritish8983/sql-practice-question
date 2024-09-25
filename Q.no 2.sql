911c743375c4ce0079883486af84fb1ad9e7b336
unknown <pritishpatra567@gmail.com>
2024-09-25 12:35:38 +0530

    second commit

---
+++ Q.no 2.sql
@@ -0,0 +1,3 @@
+insert into employee_details(firstname,Lastname,Email,phonenumber,Hiredate,Salary,Departmentid,Isactive,Jobtitle) 
+values ('Pritish','patra','pritishpatra567@gmail.com','8983732311','2019-02-15',20000,8,false,'HRhead'),
+('suresh','nikose','Suresh567@gmail.com','8983732388','2020-02-15',20000,8,false,' Softwaredeveloper');
